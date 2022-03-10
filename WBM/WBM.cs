4dc04d25da0546ecc342edea088601b6a4c4a5c6
developomp <developomp@gmail.com>
2022-03-10 15:54:45 +0900

    code splitting for key presses

--- WBM/WBM.cs
+++ WBM/WBM.cs
@@ -34,7 +34,6 @@ namespace WBM
             // Audio
             //
 
-
             this.killStreakAudioSource = this.gameObject.AddComponent<AudioSource>();
 
             if (!Directory.Exists(this.audioPath))
@@ -57,7 +56,6 @@ namespace WBM
             this.newAKSound = this.AKSoundRaw.ADCOCHNNCHM;
             this.newSMGSound = this.SMGSoundRaw.ADCOCHNNCHM;
 
-
             //
             // Websocket
             //
@@ -73,55 +71,21 @@ namespace WBM
 
         private void Update()
         {
-            // Move UI
-            if (Input.GetKey(KeyCode.LeftControl))
-            {
-                // move GUI
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    if (Input.GetKey(KeyCode.UpArrow)) this.GUIOffsetY.Value -= 1;
-                    if (Input.GetKey(KeyCode.DownArrow)) this.GUIOffsetY.Value += 1;
-                    if (Input.GetKey(KeyCode.LeftArrow)) this.GUIOffsetX.Value -= 1;
-                    if (Input.GetKey(KeyCode.RightArrow)) this.GUIOffsetX.Value += 1;
-                }
-                else
-                {
-                    if (Input.GetKeyDown(KeyCode.UpArrow)) this.GUIOffsetY.Value -= 1;
-                    if (Input.GetKeyDown(KeyCode.DownArrow)) this.GUIOffsetY.Value += 1;
-                    if (Input.GetKeyDown(KeyCode.LeftArrow)) this.GUIOffsetX.Value -= 1;
-                    if (Input.GetKeyDown(KeyCode.RightArrow)) this.GUIOffsetX.Value += 1;
-                }
-            }
-
-            // reset GUI position
-            if (this.resetGUIShortcut.Value.IsDown())
-            {
-                this.GUIOffsetX.Value = (int)this.GUIOffsetX.DefaultValue;
-                this.GUIOffsetY.Value = (int)this.GUIOffsetY.DefaultValue;
-            }
-
-            if (this.showGUIShortcut.Value.IsDown()) this.showGUI.Value = !this.showGUI.Value;
-            if (this.shiftToCrouchShortcut.Value.IsDown()) this.shiftToCrouch.Value = !this.shiftToCrouch.Value;
-            if (this.killStreakSFXShortcut.Value.IsDown()) this.killStreakSFX.Value = !this.killStreakSFX.Value;
-            if (this.showPlayerStatsShortcut.Value.IsDown()) this.showPlayerStats.Value = !this.showPlayerStats.Value;
-            if (this.showWeaponStatsShortcut.Value.IsDown()) this.showWeaponStats.Value = !this.showWeaponStats.Value;
-            if (this.showTeamStatsShortcut.Value.IsDown()) this.showTeamStats.Value = !this.showTeamStats.Value;
-            if (this.showEloOnLeaderboardShortcut.Value.IsDown()) this.showEloOnLeaderboard.Value = !this.showEloOnLeaderboard.Value;
-            if (this.showSquadServerShortcut.Value.IsDown()) this.showSquadServer.Value = !this.showSquadServer.Value;
-            if (this.showTestingServerShortcut.Value.IsDown()) this.showTestingServer.Value = !this.showTestingServer.Value;
-            if (this.clearChatShortcut.Value.IsDown()) this.clearChat();
-            if (this.clearDeathLogShortcut.Value.IsDown()) this.clearMessagesFuncRef.Invoke(this.webguy, new object[] { });
-
-            // config visibility
-            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightShift)) this._showConfig = true;
-            if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightShift)) this._showConfig = false;
-
-            // only if right buttton is not held
-            if (this.shiftToCrouch.Value && !Input.GetMouseButton(1))
-            {
-                if (Input.GetKeyDown(KeyCode.LeftShift)) OMOJPGNNKFN.NEELEHFDKBP.EGACOOOGDDC = true;
-                if (Input.GetKeyUp(KeyCode.LeftShift)) OMOJPGNNKFN.NEELEHFDKBP.EGACOOOGDDC = false;
-            }
+            this.moveUIOnKeyPress();
+            this.resetUIOnKeyPress();
+            this.toggleUIOnKeyPress();
+            this.toggleShiftToCrouchOnKeyPress();
+            this.toggleKillStreakSFXOnKeyPress();
+            this.togglePlayerStatsOnKeyPress();
+            this.toggleWeaponStatsOnKeyPress();
+            this.toggleTeamStatsOnKeyPress();
+            this.toggleLeaderboardEloOnKeyPress();
+            this.toggleSquadServerOnKeyPress();
+            this.toggleTestingServerOnKeyPress();
+            this.clearChatOnKeyPress();
+            this.clearDeathLogOnKeyPress();
+            this.showConfigOnKeyPress();
+            this.crouchOnKeyPress();
         }
 
         private void OnGUI()
@@ -273,7 +237,7 @@ total kills: {teamTotalKills}"
         }
 
         //
-        //
+        // setup functions
         //
 
         private void fetchReferences(System.Type webguyType)
